4341719dc7a3d77d434afbcbf13a98e437863179
Sekito <s20g474@stu.kagawa-u.ac.jp>
2021-06-25 13:02:33 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # fixpoint_rogramming_exam
+
+## 環境
+
+- Python 3.8.3
+
+### 動作確認
+
+||PC 1|
+|---|---|
+|OS|Windows 10 Home|
+|CPU|Intel Core i5-7200 2.50GHz|
+|メモリ|8.00GB DDR4|
+
+## 使用手順
+
+### 実行ファイルから使用
+
+- 
+
+## 関数
+
+## 大まかな処理の流れ
+
+1. シミュレータ起動
+2. SimulationManagerが時間を一時停止
+3. SettingManagerがユーザの入力を待機
+4. ユーザからの入力に合わせてSettingManagerがシミュレータに設定を反映
+5. ユーザからシミュレーション開始命令を受け取ると、SimulationManagerがシミュレーションを開始
+6. HistoryManagerはシミュレーション中に指定した時間間隔でログを記録
+7. SimulationManagerはシミュレーション中にドローンの状態を監視
+8. 全てのドローンが飛行を終了するとSimulationManagerがシミュレーションを終了
+
+## ディレクトリ構造
+
+### script
+
+- 要求に対する解答のプログラム
+
+### security_log
+
+- ログデータ
--- script/answer.py
+++ script/answer.py
@@ -3,10 +3,11 @@ import numpy as np
 
 class Answer:
     # 初期処理
-    def __init__(self):
+    def __init__(self, N):
         self.folder_name = "security_log" # フォルダー名
         self.file_name = "log_1.txt" # ログのファイル名
         self.df_log = self.get_log() # ログを取得
+        self.N = N # 故障とみなすタイムアウト回数の上限値
         
     
     # ログの取得
@@ -28,8 +29,12 @@ class Answer:
             print("error_server_address: ", address)
 
             df_error_server = self.df_log[self.df_log["address"] == address].reset_index()
-            # 故障したタイミングの行番号
+            # errorしたタイミングの行番号
             error_index_arr = df_error_server[df_error_server["response_time"] == "-"].index.values
+            # 故障の判別
+            if self.is_failure(error_index_arr):
+                print("fail")
+                continue
 
             # 復旧したタイミングの行番号
             restore_index_arr = error_index_arr + 1
@@ -45,22 +50,43 @@ class Answer:
             self.output_error_time(new_error_index_arr, new_restore_index_arr, df_error_server)
         return 0
     
-    # 故障期間を取得 (復旧時刻はないデータは考慮しない)
+    # error期間を出力 (復旧時刻はないデータは考慮しない)
     def output_error_time(self, error_arr, restore_arr, df):
         for error_index, restore_index in zip(error_arr, restore_arr):
             print("error_time: ", df.at[df.index[error_index], "date"], " - ",
                 df.at[df.index[restore_index], "date"], "\n")
 
+    # 故障の判定
+    def is_failure(self, error_index_arr):
+        # 
+        if len(error_index_arr) <= self.N:
+            return False
 
-test = Answer()
+    def is_overload():
+
+        count = 1
+        prev_num = error_index_arr[0]
+        for num in error_index_arr[1:]:
+            if (prev_num + 1) == num:
+                count += 1
+            else:
+                count = 1
+            prev_num = num
+        return count > self.N
+
+# タイムアウト回数
+N = 2
+
+test = Answer(N)
 address = test.get_error_server_address()
 test.output_error_data(address)
+
 
 # import numpy as np
 # length = 8
 # a = np.array([1, 4, 5, 7])
 # b = np.array([2, 5, 6, 7])
 # print(np.intersect1d(a, b))
-
+# print(a[1:])
 # print(np.setdiff1d(b, a))
 # log = get_log()
