3f87602890b618b04b70def8289d5cf691b2364a
Sekito <s20g474@stu.kagawa-u.ac.jp>
2021-06-25 14:47:04 +0900

    Add files via upload

--- script/answer.py
+++ script/answer.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import numpy as np
 
-class Answer:
+class LogAnalysis:
     # 初期処理
-    def __init__(self, N):
-        self.folder_name = "security_log" # フォルダー名
-        self.file_name = "log_1.txt" # ログのファイル名
-        self.df_log = self.get_log() # ログを取得
-        self.N = N # 故障とみなすタイムアウト回数の上限値
-        
+    def __init__(self, N, m, t):
+        # フォルダ・ファイル名
+        self.folder_name = "security_log"
+        self.file_name = "log_1.txt"
+        # パラメータ
+        self.N = N
+        self.m = m
+        self.t = t
+        # ログを取得
+        self.df_log = self.get_log()
     
     # ログの取得
     def get_log(self):
@@ -16,25 +20,34 @@ class Answer:
                             names=["date", "address", "response_time"],
                             sep=",")
 
-    # 故障しているサーバのアドレスを取得
-    def get_error_server_address(self):
-        """ 重複の対処 """
+    # 全サーバのアドレスを取得
+    def get_all_address(self):
+        return self.df_log["address"].unique()
+
+    # タイムアウトしたサーバのアドレスを取得
+    def get_timeout_server_address(self):
         return self.df_log[self.df_log["response_time"] == "-"]["address"].unique()
 
     # 故障しているサーバのアドレスと期間を出力
-    def output_error_data(self, address_list):
+    def output_error_data(self):
+
+        # タイムアウトしたサーバのアドレス取得
+        address_list = self.get_timeout_server_address()
+
         # サーバごとにデータフレームをまとめる
         for address in address_list:
             # アドレス出力
             print("error_server_address: ", address)
 
+            # タイムアウトが発生したサーバのデータフレームを取得
             df_error_server = self.df_log[self.df_log["address"] == address].reset_index()
             # errorしたタイミングの行番号
             error_index_arr = df_error_server[df_error_server["response_time"] == "-"].index.values
-            # 故障の判別
+
+            # 故障の判定 (故障の場合、故障時間の出力を無視)
             if self.is_failure(error_index_arr):
                 print("fail")
-                continue
+                continue            
 
             # 復旧したタイミングの行番号
             restore_index_arr = error_index_arr + 1
@@ -50,6 +63,8 @@ class Answer:
             self.output_error_time(new_error_index_arr, new_restore_index_arr, df_error_server)
         return 0
     
+
+    
     # error期間を出力 (復旧時刻はないデータは考慮しない)
     def output_error_time(self, error_arr, restore_arr, df):
         for error_index, restore_index in zip(error_arr, restore_arr):
@@ -58,12 +73,16 @@ class Answer:
 
     # 故障の判定
     def is_failure(self, error_index_arr):
-        # 
+        # タイムアウトの回数が上限値以下の場合
         if len(error_index_arr) <= self.N:
             return False
+        
+        # タイムアウトの連続値をカウントし故障かどうか判別
+        if self.count_timeout(error_index_arr) > self.N:
+            return True
 
-    def is_overload():
-
+    # タイムアウトの連続値をカウント
+    def count_timeout(self, error_index_arr):
         count = 1
         prev_num = error_index_arr[0]
         for num in error_index_arr[1:]:
@@ -72,21 +91,45 @@ class Answer:
             else:
                 count = 1
             prev_num = num
-        return count > self.N
+        return count
+    
+    # サーバの過負荷をチェック
+    def output_overload(self):
+        # 全サーバを走査
+        for address in self.get_all_address():
+            df_one_server = self.df_log[self.df_log["address"] == address].reset_index()
 
-# タイムアウト回数
-N = 2
+            # 平均応答時間
+            mean_response_time = self.get_mean_response_time(df_one_server)
 
-test = Answer(N)
-address = test.get_error_server_address()
-test.output_error_data(address)
+            # 過負荷の判定
+            if self.is_overload(mean_response_time):
+                # サーバのアドレスを出力
+                print("overload: {}".format(address))
+        return None
 
+    # 過負荷の判定
+    def is_overload(self, mean_response_time):
+        return mean_response_time > self.t
 
-# import numpy as np
-# length = 8
-# a = np.array([1, 4, 5, 7])
-# b = np.array([2, 5, 6, 7])
-# print(np.intersect1d(a, b))
-# print(a[1:])
-# print(np.setdiff1d(b, a))
-# log = get_log()
+    # 直近m個のデータの平均応答時間を取得
+    def get_mean_response_time(self, df):
+        return df.iloc[-self.m:].mean()["response_time"]
+        
+
+# メイン処理
+def main():
+    # パラメータ
+    N = 3   # 連続タイムアウトの上限
+    m = 2   # 直近のデータ数
+    t = 2   # 過負荷判定の基準となる応答時間[ミリ秒]
+
+    la = LogAnalysis(N, m, t)
+    # 設問1, 2
+    la.output_error_data()
+    # 設問3
+    la.output_overload()
+
+# おまじない(正しくファイルが実行されているかどうか)
+if __name__ == "__main__":
+    main()
